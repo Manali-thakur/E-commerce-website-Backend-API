ae34e332a81f1a0bdaca7d60bdce6b4eea392147
Manali-thakur <manalithakur7062@gmail.com>
2026-08-09 19:03:54 +0530

    update

---
+++ src/features/product/controller/product.conttroller.js
@@ -0,0 +1,24 @@
+
+export class ProductController {
+  // add, all, rate, getone, filter
+
+  getAllProducts(req, res) {
+    // code
+  }
+
+  addProduct(req, res) {
+    // code
+  }
+
+  getOneProduct(req, res) {
+    // code
+  }
+
+  rateProduct(req, res) {
+    // code
+  }
+
+  filterProducts(req, res) {
+    // code
+  }
+}
