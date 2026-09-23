e8958bec20857ae077011f31b2581ed39d276e4e
Manali-thakur <manalithakur7062@gmail.com>
2026-09-24 03:06:29 +0530

    sessionTransaction is working using the replicaset of the mongodb

--- src/features/order/order.repository.js
+++ src/features/order/order.repository.js
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb";
-import { getDB } from "../../../config/mongodb.js";
+import { getClient, getDB } from "../../../config/mongodb.js";
 import OrderModel from "./order.model.js";
 import { ApplicationError } from "../../error-handler/applicationError.js";
 
@@ -9,11 +9,15 @@ export default class OrderRepository {
   }
 
   async placeOrder(userId) {
+    const db = await getDB();
+    const client = await getClient();
+    const session = client.startSession();
+
     try {
-      const db = await getDB();
+      await session.startTransaction();
 
       // 1, get cartitems and calculate total amount
-      const items = await this.getTotalAmount(userId);
+      const items = await this.getTotalAmount(userId, session);
 
       //total amount of all the cart items acc-accumulator
       const finalTotalAmount = items.reduce(
@@ -28,7 +32,7 @@ export default class OrderRepository {
         finalTotalAmount,
         new Date(),
       );
-      await db.collection(this.collection).insertOne(newOrder);
+      await db.collection(this.collection).insertOne(newOrder, { session });
 
       // 3. reduce the stock
       for (let item of items) {
@@ -39,57 +43,72 @@ export default class OrderRepository {
           {
             $inc: { stock: -item.quantity },
           },
+          { session },
         );
       }
-      throw new Error("something");
+      // throw new Error("something");
 
       // 4. clear the cart items.
-      await db.collection("cartItems").deleteMany({
-        userID: new ObjectId(userId),
-      });
+      await db.collection("cartItems").deleteMany(
+        {
+          userID: new ObjectId(userId),
+        },
+        { session },
+      );
+
+      // imp to add of nothing will be saved or done
+      await session.commitTransaction();
 
       return;
     } catch (err) {
-      // console.log(
-      //   "error during place order repository--------------------",
-      //   err,
-      // );
+      await session.abortTransaction();
+
+      console.log(
+        "error during place order repository-------------------------------",
+        err,
+        "------------------------------------",
+      );
       throw new ApplicationError("Something went wrong with the database", 500);
+    } finally {
+      await session.endSession();
     }
   }
 
-  async getTotalAmount(userId) {
+  async getTotalAmount(userId, session) {
     const db = await getDB();
 
     const items = await db
       .collection("cartItems")
-      .aggregate([
-        // 1.get all cart items for the user
-        {
-          $match: { userID: new ObjectId(userId) },
-        },
-        // 2.Get the products from product collection
-        {
-          $lookup: {
-            from: "products",
-            localField: "productID",
-            foreignField: "_id",
-            as: "productInfo",
+      .aggregate(
+        [
+          // 1.get all cart items for the user
+          {
+            $match: { userID: new ObjectId(userId) },
           },
-        },
-        // 3. Unwind the productinfo retrieve all cart items
-        {
-          $unwind: "$productInfo",
-        },
-        // 4.calculate totalAmount for each cartitems
-        {
-          $addFields: {
-            totalAmount: {
-              $multiply: ["$productInfo.price", "$quantity"],
+          // 2.Get the products from product collection
+          {
+            $lookup: {
+              from: "products",
+              localField: "productID",
+              foreignField: "_id",
+              as: "productInfo",
             },
           },
-        },
-      ])
+          // 3. Unwind the productinfo retrieve all cart items
+          {
+            $unwind: "$productInfo",
+          },
+          // 4.calculate totalAmount for each cartitems
+          {
+            $addFields: {
+              totalAmount: {
+                $multiply: ["$productInfo.price", "$quantity"],
+              },
+            },
+          },
+        ],
+        { session },
+      )
       .toArray();
 
     return items;
